932fbc9106337c62af50782c51a88f46aa7a3c79
Andrew Higgins <ahiggins@palantir.com>
2019-02-20 17:19:31 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="lispy",
+    description="Python Distribution Utilities",
+    author="Andrew Higgins",
+    url="https://github.com/ahggns/lispy",
+    packages=find_packages(),
+)
